594c4bd4289869da669f2c38f4d2d9d46c71c584
Sergey O <so@fas.harvard.edu>
2021-08-11 13:35:09 -0400

    Update mmseqs2.py

--- beta/mmseqs2.py
+++ beta/mmseqs2.py
@@ -4,10 +4,16 @@ import time
 import requests
 import random
 
-def run_mmseqs2(query_sequence, prefix, use_env=True, filter=False):
+def run_mmseqs2(x, prefix, use_env=True, filter=False):
   
-  def submit(query_sequence, mode, N=1):
-    res = requests.post('https://a3m.mmseqs.com/ticket/msa', data={'q':f">{N}\n{query_sequence}", 'mode': mode})
+  def submit(seqs, mode, N=1):
+    
+    n,query = N,""
+    for seq in seqs:
+      query += f">{n}\n{seq}\n"
+      n += 1
+      
+    res = requests.post('https://a3m.mmseqs.com/ticket/msa', data={'q':query,'mode': mode})
     try: out = res.json()
     except ValueError: out = {"status":"UNKNOWN"}
     return out
@@ -21,12 +27,17 @@ def run_mmseqs2(query_sequence, prefix, use_env=True, filter=False):
   def download(ID, path):
     res = requests.get(f'https://a3m.mmseqs.com/result/download/{ID}')
     with open(path,"wb") as out: out.write(res.content)
+  
+  # process input x
+  seqs = [x] if isinstance(x, str) else x
     
+  # setup mode
   if filter:
     mode = "env" if use_env else "all"
   else:
     mode = "env-nofilter" if use_env else "nofilter"
   
+  # define path
   path = f"{prefix}_{mode}"
   if not os.path.isdir(path): os.mkdir(path)
 
@@ -39,11 +50,11 @@ def run_mmseqs2(query_sequence, prefix, use_env=True, filter=False):
         pbar.set_description("SUBMIT")
         
         # Resubmit job until it goes through
-        out = submit(query_sequence, mode, N)
+        out = submit(seqs, mode, N)
         while out["status"] in ["UNKNOWN","RATELIMIT"]:
           # resubmit
           time.sleep(5 + random.randint(0,5))
-          out = submit(query_sequence, mode, N)
+          out = submit(seqs, mode, N)
 
         # wait for job to finish
         ID,TIME = out["id"],0
@@ -69,20 +80,28 @@ def run_mmseqs2(query_sequence, prefix, use_env=True, filter=False):
       # Download results
       download(ID, tar_gz_file)
 
-  # parse a3m files
-  a3m_lines = []
-  a3m = f"{prefix}_{mode}.a3m"
-  if not os.path.isfile(a3m):
-    with tarfile.open(tar_gz_file) as tar_gz: tar_gz.extractall(path)
-    a3m_files = [f"{path}/uniref.a3m"]
-    if use_env: a3m_files.append(f"{path}/bfd.mgnify30.metaeuk30.smag30.a3m")
-    a3m_out = open(a3m,"w")
-    for a3m_file in a3m_files:
-      for line in open(a3m_file,"r"):
-        line = line.replace("\x00","")
-        if len(line) > 0:
-          a3m_lines.append(line)
-          a3m_out.write(line)
+  # prep list of a3m files
+  a3m_files = [f"{path}/uniref.a3m"]
+  if use_env: a3m_files.append(f"{path}/bfd.mgnify30.metaeuk30.smag30.a3m")
+  
+  # extract a3m files
+  if not os.path.isfile(a3m_files[0])
+    with tarfile.open(tar_gz_file) as tar_gz:
+      tar_gz.extractall(path)  
+    
+  a3m_lines = {n:[] for n in range(N,N+len(seqs))}
+  for a3m_file in a3m_files:
+    update_M,M = True,None
+    for line in open(a3m_file,"r"):
+      if len(line) > 0:
+        if "\x00" in line:
+          update_M = True
+        else:
+          if line.startswith(">") and update_M:
+            M = int(line[1:].rstrip())
+            update_M = False
+          a3m_lines[M].append(line)
+  if isinstance(x, str):
+    return "".join(a3m_lines[N])
   else:
-    a3m_lines = open(a3m).readlines()
-  return "".join(a3m_lines)
+    return ["".join(a3m_lines[n]) for n in range(N,N+len(x))]
