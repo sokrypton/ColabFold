0459a1385d951392ae535ad23ef29f942e3aacb4
konstin <konstin@mailbox.org>
2021-09-16 13:01:07 +0900

    Poetry update

--- colabfold/utils.py
+++ colabfold/utils.py
@@ -2,6 +2,14 @@ import logging
 
 from absl import logging as absl_logging
 
+NO_GPU_FOUND = """ERROR: Jax could not find GPU. This can be either because your machine doesn't have a GPU
+or because jax can't find it. You might need to run
+
+pip install --upgrade "jax[cuda111]" -f https://storage.googleapis.com/jax-releases/jax_releases.html  # Note: wheels only available on linux.
+
+See https://github.com/google/jax/#pip-installation-gpu-cuda for more details.
+
+If you're sure you want to run without a GPU, pass `--cpu`"""
 
 class TqdmHandler(logging.StreamHandler):
     """https://stackoverflow.com/a/38895482/3549270"""
@@ -25,11 +33,3 @@ def setup_logging():
     absl_logging.set_verbosity("error")
 
 
-NO_GPU_FOUND = """ERROR: Jax could not find GPU. This can be either because your machine doesn't have a GPU
-or because jax can't find it. You might need to run
-
-pip install --upgrade "jax[cuda111]" -f https://storage.googleapis.com/jax-releases/jax_releases.html  # Note: wheels only available on linux.
-
-See https://github.com/google/jax/#pip-installation-gpu-cuda for more details.
-
-If you're sure you want to run without a GPU, pass `--cpu`"""
