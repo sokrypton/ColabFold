68bf7bb738f9f69d409f5147e6aebe43bcf8f6f9
Sergey O <so@fas.harvard.edu>
2021-11-14 14:40:27 -0500

    Update colabfold_alphafold.py
    applying patch from @sjdv1982

--- beta/colabfold_alphafold.py
+++ beta/colabfold_alphafold.py
@@ -528,8 +528,8 @@ def prep_feats(I, clean=False):
   def _placeholder_template_feats(num_templates_, num_res_):
     return {
         'template_aatype': np.zeros([num_templates_, num_res_, 22], np.float32),
-        'template_all_atom_masks': np.zeros([num_templates_, num_res_, 37, 3], np.float32),
-        'template_all_atom_positions': np.zeros([num_templates_, num_res_, 37], np.float32),
+        'template_all_atom_masks': np.zeros([num_templates_, num_res_, 37], np.float32),
+        'template_all_atom_positions': np.zeros([num_templates_, num_res_, 37, 3], np.float32),
         'template_domain_names': np.zeros([num_templates_], np.float32),
         'template_sum_probs': np.zeros([num_templates_], np.float32),
     }
