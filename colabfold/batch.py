360c57996e4df28f1878770821923f1056e2c4ed
konstin <konstin@mailbox.org>
2021-11-24 00:44:39 +0900

    Don't read non-fasta/a3m from directory for #97

--- colabfold/batch.py
+++ colabfold/batch.py
@@ -376,6 +376,9 @@ def get_queries(
         for file in sorted(input_path.iterdir()):
             if not file.is_file():
                 continue
+            if file.suffix.lower() not in [".a3m", ".fasta"]:
+                logger.warning(f"non-fasta/a3m file in input directory: {file}")
+                continue
             (seqs, header) = parse_fasta(file.read_text())
             if len(seqs) == 0:
                 logger.error(f"{file} is empty")
