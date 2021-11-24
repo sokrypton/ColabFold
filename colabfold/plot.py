4030fd23d529d5bb05c0b3d5c82617cb866c4672
samuelmurail <samuel.murail@univ-paris-diderot.fr>
2021-11-24 10:19:59 +0100

    Remove copied function from colabfold.py, replace it by an import

--- colabfold/plot.py
+++ colabfold/plot.py
@@ -6,51 +6,15 @@ from matplotlib import collections as mcoll
 import matplotlib
 from string import ascii_uppercase, ascii_lowercase
 
-pymol_color_list = [
-    "#33ff33",
-    "#00ffff",
-    "#ff33cc",
-    "#ffff00",
-    "#ff9999",
-    "#e5e5e5",
-    "#7f7fff",
-    "#ff7f00",
-    "#7fff7f",
-    "#199999",
-    "#ff007f",
-    "#ffdd5e",
-    "#8c3f99",
-    "#b2b2b2",
-    "#007fff",
-    "#c4b200",
-    "#8cb266",
-    "#00bfbf",
-    "#b27f7f",
-    "#fcd1a5",
-    "#ff7f7f",
-    "#ffbfdd",
-    "#7fffff",
-    "#ffff7f",
-    "#00ff7f",
-    "#337fcc",
-    "#d8337f",
-    "#bfff3f",
-    "#ff7fff",
-    "#d8d8ff",
-    "#3fffbf",
-    "#b78c4c",
-    "#339933",
-    "#66b2b2",
-    "#ba8c84",
-    "#84bf00",
-    "#b24c66",
-    "#7f7f7f",
-    "#3f3fa5",
-    "#a5512b",
-]
-
-pymol_cmap = matplotlib.colors.ListedColormap(pymol_color_list)
-alphabet_list = list(ascii_uppercase + ascii_lowercase)
+from colabfold.colabfold import (
+    pymol_color_list,
+    pymol_cmap,
+    alphabet_list,
+    kabsch,
+    plot_pseudo_3D,
+    add_text,
+    plot_ticks,
+)
 
 
 def plot_predicted_alignment_error(
@@ -110,134 +74,6 @@ def plot_lddt(
     if show:
         plt.show()
     plt.close()
-
-
-def kabsch(a, b, weights=None, return_v=False):
-    a = np.asarray(a)
-    b = np.asarray(b)
-    if weights is None:
-        weights = np.ones(len(b))
-    else:
-        weights = np.asarray(weights)
-    B = np.einsum("ji,jk->ik", weights[:, None] * a, b)
-    u, s, vh = np.linalg.svd(B)
-    if np.linalg.det(u @ vh) < 0:
-        u[:, -1] = -u[:, -1]
-    if return_v:
-        return u
-    else:
-        return u @ vh
-
-
-def plot_pseudo_3D(
-    xyz,
-    c=None,
-    ax=None,
-    chainbreak=5,
-    cmap="gist_rainbow",
-    line_w=2.0,
-    cmin=None,
-    cmax=None,
-    zmin=None,
-    zmax=None,
-):
-    def rescale(a, amin=None, amax=None):
-        a = np.copy(a)
-        if amin is None:
-            amin = a.min()
-        if amax is None:
-            amax = a.max()
-        a[a < amin] = amin
-        a[a > amax] = amax
-        return (a - amin) / (amax - amin)
-
-    # make segments
-    xyz = np.asarray(xyz)
-    seg = np.concatenate([xyz[:-1, None, :], xyz[1:, None, :]], axis=-2)
-    seg_xy = seg[..., :2]
-    seg_z = seg[..., 2].mean(-1)
-    ord = seg_z.argsort()
-
-    # set colors
-    if c is None:
-        c = np.arange(len(seg))[::-1]
-    else:
-        c = (c[1:] + c[:-1]) / 2
-    c = rescale(c, cmin, cmax)
-
-    if isinstance(cmap, str):
-        if cmap == "gist_rainbow":
-            c *= 0.75
-        colors = matplotlib.cm.get_cmap(cmap)(c)
-    else:
-        colors = cmap(c)
-
-    if chainbreak is not None:
-        dist = np.linalg.norm(xyz[:-1] - xyz[1:], axis=-1)
-        colors[..., 3] = (dist < chainbreak).astype(np.float)
-
-    # add shade/tint based on z-dimension
-    z = rescale(seg_z, zmin, zmax)[:, None]
-    tint, shade = z / 3, (z + 2) / 3
-    colors[:, :3] = colors[:, :3] + (1 - colors[:, :3]) * tint
-    colors[:, :3] = colors[:, :3] * shade
-
-    set_lim = False
-    if ax is None:
-        fig, ax = plt.subplots()
-        fig.set_figwidth(5)
-        fig.set_figheight(5)
-        set_lim = True
-    else:
-        fig = ax.get_figure()
-        if ax.get_xlim() == (0, 1):
-            set_lim = True
-
-    if set_lim:
-        xy_min = xyz[:, :2].min() - line_w
-        xy_max = xyz[:, :2].max() + line_w
-        ax.set_xlim(xy_min, xy_max)
-        ax.set_ylim(xy_min, xy_max)
-
-    ax.set_aspect("equal")
-
-    # determine linewidths
-    width = fig.bbox_inches.width * ax.get_position().width
-    linewidths = line_w * 72 * width / np.diff(ax.get_xlim())
-
-    lines = mcoll.LineCollection(
-        seg_xy[ord],
-        colors=colors[ord],
-        linewidths=linewidths,
-        path_effects=[matplotlib.patheffects.Stroke(capstyle="round")],
-    )
-
-    return ax.add_collection(lines)
-
-
-def add_text(text, ax):
-    return plt.text(
-        0.5,
-        1.01,
-        text,
-        horizontalalignment="center",
-        verticalalignment="bottom",
-        transform=ax.transAxes,
-    )
-
-
-def plot_ticks(Ls):
-    Ln = sum(Ls)
-    L_prev = 0
-    for L_i in Ls[:-1]:
-        L = L_prev + L_i
-        L_prev += L_i
-        plt.plot([0, Ln], [L, L], color="black")
-        plt.plot([L, L], [0, Ln], color="black")
-    ticks = np.cumsum([0] + Ls)
-    ticks = (ticks[1:] + ticks[:-1]) / 2
-    plt.yticks(ticks, alphabet_list[: len(ticks)])
-
 
 def plot_protein_confidence(
     plot_path,
@@ -334,8 +170,3 @@ def plot_protein_confidence(
     if show:
         plt.show()
     plt.close()
-
-
-# fig = plot_protein_confidence(unrelaxed_protein,
-#                              pae=prediction_result['predicted_aligned_error'],
-#                              Ls=query_sequence_len_array)
