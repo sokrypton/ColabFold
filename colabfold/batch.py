e32a7f516200cafe27949404addd0fab8dabcbfb
Martin Steinegger <themartinsteinegger@gmail.com>
2021-12-02 11:55:37 +0900

    Allow paired only MSA mode for AlphaFold2-multimer

--- colabfold/batch.py
+++ colabfold/batch.py
@@ -698,9 +698,13 @@ def generate_input_feature(
         chain_cnt = 0
         for sequence_index, sequence in enumerate(query_seqs_unique):
             for cardinality in range(0, query_seqs_cardinality[sequence_index]):
+                input_msa = [">" + str(101 + sequence_index) + "\n" + sequence]
+                if unpaired_msa is not None:
+                    input_msa = unpaired_msa[sequence_index]
+                
                 feature_dict = build_monomer_feature(
                     sequence,
-                    unpaired_msa[sequence_index],
+                    input_msa,
                     template_features[sequence_index],
                 )
                 if is_complex:
