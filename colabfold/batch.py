b06cdd53af2aab5a2564eb22910e5df770706236
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2022-01-26 03:56:49 +0900

    Black issue fix

--- colabfold/batch.py
+++ colabfold/batch.py
@@ -1,6 +1,6 @@
 import os
-os.environ['TF_FORCE_UNIFIED_MEMORY'] = "1"
-os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = "2.0"
+os.environ["TF_FORCE_UNIFIED_MEMORY"] = "1"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "2.0"
 
 import json
 import logging
