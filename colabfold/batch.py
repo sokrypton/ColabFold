4079b0171f1ec87e628546dd025984b84be37483
YoshitakaMo <virgospica93@gmail.com>
2022-08-16 11:27:42 +0900

    add '--max-msa 16:32'

--- colabfold/batch.py
+++ colabfold/batch.py
@@ -1670,7 +1670,7 @@ def main():
         help="defines: `max_msa_clusters:max_extra_msa` number of sequences to use",
         type=str,
         default=None,
-        choices=["512:5120", "512:1024", "256:512", "128:256", "64:128", "32:64"],
+        choices=["512:5120", "512:1024", "256:512", "128:256", "64:128", "32:64", "16:32"],
     )
 
     parser.add_argument(
