c3e204e9878c281c771aca4c086b2d38608b5382
Sergey O <so@fas.harvard.edu>
2023-02-05 10:15:04 -0500

    fix test

--- tests/reindent_ipynb.py
+++ tests/reindent_ipynb.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 if __name__ == "__main__":
-    for notebook in Path(".").rglob("*.ipynb"):
-        print(notebook)
-        data = json.loads(open(notebook).read())
-        open(notebook, "w").write(json.dumps(data, indent=2, ensure_ascii=False))
+  for notebook in Path(".").rglob("*.ipynb"):
+    print(notebook)
+    data = json.loads(open(notebook).read())
+    open(notebook, "w").write(json.dumps(data, indent=2, ensure_ascii=False))
