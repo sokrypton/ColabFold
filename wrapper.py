a9cc3a0dd797c0a4b4aeb423cf2f03e80ab911d3
lcirne <lcirne@uvm.edu>
2025-06-10 15:41:43 -0400

    Built job logging system using json, currently only has JID and user but can be easily expanded to store pdb file data and other useful information for model recycling.

--- wrapper.py
+++ wrapper.py
@@ -7,16 +7,48 @@ Ma Lab
 import os
 import sys
 import json
+import getpass
 
 def initialize_project(jobs):
     try:
+        username = getpass.getuser()
+        print(f">>> LOGGING AS: {username}")
+    except OSError:
+        print(">>> COULD NOT OBTAIN USERNAME")
+        print(">>> RESULTS STORED UNDER DEFAULT")
+        username = "DEFUALT"
+
+    try:
         with open(jobs, "r") as file:
-            print("reading")
+            print(">>> READING JSON")
             job_dict = json.load(file)
-            
+            user_JIDs = [int(job["JID"]) for job in job_dict["jobs"] if job["user"] == username]
+            last_user_JID = max(user_JIDs) if user_JIDs else 0
+            new_job = {
+                "user": username,
+                "JID": last_user_JID + 1
+            }
+            job_dict["jobs"].append(new_job)
+        
+        with open(jobs, 'w') as f:
+            print(">>> APPENDING TO JSON")
+            json.dump(job_dict, f, indent=4)
+            print("APPEND COMPLETE")
+
     except FileNotFoundError:
         with open(jobs, "w") as file:
-            print("writing")
+            print(">>> WRITING NEW JSON...")
+            new_json = '''
+                {
+                    "jobs": [
+                        {
+                            "user": %(username)s,
+                            "JID": 0
+                        }
+                    ]
+                }
+            ''' % {"username": username}
+            print("WRITE COMPLETE")
 
 
 def main():
@@ -26,6 +58,8 @@ def main():
     print((" " * 7) + "ColabFold Wrapper")
     print()
     print("*" * 31)
+
+
 
 
 if __name__ == '__main__':
