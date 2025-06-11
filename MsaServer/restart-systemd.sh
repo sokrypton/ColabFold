00943d6a229d87ac8c9f10d58062e15862aefbbe
lcirne <lcirne@uvm.edu>
2025-06-11 13:39:05 -0400

    Separated json writing functionality from initialize_project() to its own function.

--- MsaServer/restart-systemd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-sudo systemctl stop mmseqs-server
-rm -rf -- jobs
-mkdir -p jobs
-sudo systemctl start mmseqs-server
--- wrapper.py
+++ wrapper.py
@@ -5,50 +5,56 @@ Template Cycling with Experimental Distance Restraint Data
 Ma Lab
 """
 import os
-import sys
+import subprocess
 import json
 import getpass
 
-def initialize_project(jobs):
+def initialize_project(jobs) -> list:
+    key_values = []
+    # Obtain username
     try:
         username = getpass.getuser()
         print(f">>> LOGGING AS: {username}")
     except OSError:
         print(">>> COULD NOT OBTAIN USERNAME")
-        print(">>> RESULTS STORED UNDER DEFAULT")
+        print(">>> LOGGING AS: DEFAULT")
         username = "DEFUALT"
+    key_values.append(("user", username))
 
+    # Obtain job ID
     try:
         with open(jobs, "r") as file:
             print(">>> READING JSON")
             job_dict = json.load(file)
             user_JIDs = [int(job["JID"]) for job in job_dict["jobs"] if job["user"] == username]
             last_user_JID = max(user_JIDs) if user_JIDs else 0
-            new_job = {
-                "user": username,
-                "JID": last_user_JID + 1
-            }
-            job_dict["jobs"].append(new_job)
-        
-        with open(jobs, 'w') as f:
-            print(">>> APPENDING TO JSON")
-            json.dump(job_dict, f, indent=4)
-            print("APPEND COMPLETE")
-
+            key_values.append(("JID", last_user_JID+1))
     except FileNotFoundError:
-        with open(jobs, "w") as file:
-            print(">>> WRITING NEW JSON...")
-            new_json = '''
-                {
-                    "jobs": [
-                        {
-                            "user": %(username)s,
-                            "JID": 0
-                        }
-                    ]
-                }
-            ''' % {"username": username}
-            print("WRITE COMPLETE")
+        key_values.append(("JID", 0))
+    append_json(jobs, key_values)
+
+    # Create shell script in same directory as wrapper.py
+    
+        
+
+def append_json(jobs, key_values):
+    # Convert list of tuples into a proper dictionary
+    new_entry = {key: value for key, value in key_values}
+
+    try:
+        with open(jobs, "r") as f:
+            job_dict = json.load(f)
+    except FileNotFoundError:
+        # Create new JSON structure if file doesn't exist
+        job_dict = {"jobs": []}
+
+    # Append the new dictionary entry
+    job_dict["jobs"].append(new_entry)
+
+    with open(jobs, "w") as f:
+        print(">>> APPENDING JSON")
+        json.dump(job_dict, f, indent=4)
+        print("###### COMPLETE ######")
 
 
 def main():
