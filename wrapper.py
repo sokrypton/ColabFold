47e208da43e4ea0c747be9774f82f0870316e25b
lcirne <lcirne@uvm.edu>
2025-06-24 11:44:43 -0400

    initialize_project() now creates an executable shell script, although experiences a raises a format error when attempting to run.

--- wrapper.py
+++ wrapper.py
@@ -9,7 +9,7 @@ import subprocess
 import json
 import getpass
 
-def initialize_project(jobs) -> list:
+def initialize_project(jobs) -> str:
     key_values = []
     # Obtain username
     try:
@@ -31,8 +31,8 @@ def initialize_project(jobs) -> list:
             current_JID = last_user_JID + 1
             key_values.append(("JID", current_JID))
     except FileNotFoundError:
-        key_values.append(("JID", 0))
-    append_json(jobs, key_values)
+        current_JID = 0
+        key_values.append(("JID", current_JID))
 
     # Obtain input file
     while True:
@@ -41,7 +41,8 @@ def initialize_project(jobs) -> list:
             break
         else:
             print("###### Invalid filepath ######")
-    
+    key_values.append(("input_file", input_file))    
+
     # Obtain num recycles
     while True:
         num_c = input("Desired number of recycles (integer) (max 5, min 0): ")
@@ -52,19 +53,20 @@ def initialize_project(jobs) -> list:
                 print("###### Invalid input ######")
         except ValueError:
                 print("###### Invalid input ######")
+    key_values.append(("num_recycles", num_c))    
     
 
     # Obtain num seeds
     while True:
-        nseed = input("Desired number of seeds (integer) (min 0): ")
+        num_s = input("Desired number of seeds (integer) (min 0): ")
         try:
-            if 0 < int(nseed):
+            if 0 < int(num_s):
                 break
             else:
                 print("###### Invalid input #######")
         except ValueError:
                 print("###### Invalid input #######")
-
+    key_values.append(("num_s", num_s))
 
     # Create shell script to run colabfold_batch
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -72,29 +74,36 @@ def initialize_project(jobs) -> list:
     script_path = os.path.join(current_dir, script_name)
     # Writing script
     script_content = f"""
-        JID={current_JID}
-        num_c={num_c}
-        seed=1
-        nseed={nseed}
-        m_e_msa=32
-        m_msa=$(($m_e_msa / 2))
-        input_file=./{input_file}
-        outputdir={username}{current_JID}mm$m_msa
-        temp_dir
+JID={current_JID}
+num_c={num_c}
+seed=1
+num_s={num_s}
+m_e_msa=32
+m_msa=$(($m_e_msa / 2))
+input_file=./{input_file}
+outputdir={username}{current_JID}mm$m_msa
+temp_dir
 
-        colabfold_batch --pair-mode unpaired_paired --templates \
-        --msa-mode mmseqs2_uniref_env \
-        --custom-template-path $temp_dir \
-        --max-msa $m_msa:$m_e_msa \
-        --use-dropout \
-        --random-seed $seed \
-        --num-seeds $nseed \
-        --num-recycle $num_c \
-        --relax-max-iterations 100 \
-        --use-gpu-relax \
-        $inputfile $outputdir
+colabfold_batch --pair-mode unpaired_paired --templates \\
+--msa-mode mmseqs2_uniref_env \\
+--custom-template-path $temp_dir \\
+--max-msa $m_msa:$m_e_msa \\
+--use-dropout \\
+--random-seed $seed \\
+--num-seeds $num_s \\
+--num-recycle $num_c \\
+--relax-max-iterations 100 \\
+--use-gpu-relax \\
+$inputfile $outputdir
     """
-     
+    # Create shell script to execute ColabFold
+    with open(script_path, 'w') as file:
+        print(">>> WRITING SHELL SCRIPT")
+        file.write(script_content)
+    os.chmod(script_path, 0o755)
+    append_json("jobs.json", key_values)
+    return script_path
+
 
 def append_json(jobs, key_values):
     # Convert list of tuples into a proper dictionary
@@ -116,6 +125,16 @@ def append_json(jobs, key_values):
         print("###### COMPLETE ######")
 
 
+def run_colabfold(script_path):
+    print(">>> ATTEMPTING RUN")
+    subprocess.run([script_path], check=True)
+    return 0
+
+
+def filter_output():
+    return 0
+
+
 def main():
     """ Welcome """
     print("*" * 31)
@@ -123,9 +142,8 @@ def main():
     print((" " * 7) + "ColabFold Wrapper")
     print()
     print("*" * 31)
-
-
-
+    
+    script_path = initialize_project("jobs.json")
 
 if __name__ == '__main__':
     main()
