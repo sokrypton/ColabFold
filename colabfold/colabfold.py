7b92135f0af1ec1ac603ca31b6fda1c646e112dc
Choonghwan Lee <chlee19990109@gmail.com>
2026-09-16 03:24:32 +0900

    Bound the MSA server retries

--- colabfold/colabfold.py
+++ colabfold/colabfold.py
@@ -89,12 +89,9 @@ def run_mmseqs2(x, prefix, use_env=True, use_filter=True,
         # https://requests.readthedocs.io/en/latest/user/advanced/#advanced
         # "good practice to set connect timeouts to slightly larger than a multiple of 3"
         res = requests.post(f'{host_url}/{submission_endpoint}', data={ 'q': query, 'mode': mode }, timeout=6.02, headers=headers)
-      except requests.exceptions.Timeout:
-        logger.warning("Timeout while submitting to MSA server. Retrying...")
-        continue
       except Exception as e:
         error_count += 1
-        logger.warning(f"Error while fetching result from MSA server. Retrying... ({error_count}/5)")
+        logger.warning(f"Error while submitting to MSA server. Retrying... ({error_count}/5)")
         logger.warning(f"Error: {e}")
         time.sleep(5)
         if error_count >= 5:
@@ -114,15 +111,12 @@ def run_mmseqs2(x, prefix, use_env=True, use_filter=True,
     while True:
       try:
         res = requests.get(f'{host_url}/ticket/{ID}', timeout=6.02, headers=headers)
-      except requests.exceptions.Timeout:
-        logger.warning("Timeout while fetching status from MSA server. Retrying...")
-        continue
       except Exception as e:
         error_count += 1
         logger.warning(f"Error while fetching result from MSA server. Retrying... ({error_count}/5)")
         logger.warning(f"Error: {e}")
         time.sleep(5)
-        if error_count > 5:
+        if error_count >= 5:
           raise
         continue
       break
@@ -138,15 +132,12 @@ def run_mmseqs2(x, prefix, use_env=True, use_filter=True,
     while True:
       try:
         res = requests.get(f'{host_url}/result/download/{ID}', timeout=6.02, headers=headers)
-      except requests.exceptions.Timeout:
-        logger.warning("Timeout while fetching result from MSA server. Retrying...")
-        continue
       except Exception as e:
         error_count += 1
         logger.warning(f"Error while fetching result from MSA server. Retrying... ({error_count}/5)")
         logger.warning(f"Error: {e}")
         time.sleep(5)
-        if error_count > 5:
+        if error_count >= 5:
           raise
         continue
       break
@@ -278,15 +269,12 @@ def run_mmseqs2(x, prefix, use_env=True, use_filter=True,
             # https://requests.readthedocs.io/en/latest/user/advanced/#advanced
             # "good practice to set connect timeouts to slightly larger than a multiple of 3"
             response = requests.get(f"{host_url}/template/{TMPL_LINE}", stream=True, timeout=6.02, headers=headers)
-          except requests.exceptions.Timeout:
-            logger.warning("Timeout while submitting to template server. Retrying...")
-            continue
           except Exception as e:
             error_count += 1
             logger.warning(f"Error while fetching result from template server. Retrying... ({error_count}/5)")
             logger.warning(f"Error: {e}")
             time.sleep(5)
-            if error_count > 5:
+            if error_count >= 5:
               raise
             continue
           break
